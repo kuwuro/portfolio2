9abd3ff216799fce7e81418f13970b6ca809e2b8
Enric Armengol <enarga@jviladoms.cat>
2024-08-14 20:22:23 +0200

    [FIX] small css fix mobile

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -33,7 +33,7 @@ function Modal({ children, onClose }) {
     const modalContent = (
         screenWidth <= 600 ? (
             <div className={`fixed bottom-0 w-full z-50 transition-opacity ${modalOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} style={{ height: '80vh' }}>
-                <div className="modal-content max-w-full md:max-w-lg lg:max-w-none w-auto h-full flex flex-col p-8 bg-white dark:bg-black rounded-lg shadow-xl dark:border dark:border-white20 border-opacity-20 bg-opacity-90 backdrop-blur-xl transition-transform transform scale-100 fade-up">
+                <div className="modal-content max-w-full md:max-w-lg lg:max-w-none w-auto h-full flex flex-col p-8 bg-white dark:bg-black rounded-lg rounded-b-none shadow-xl dark:border-2 dark:border-b-0 dark:border-white20 border-opacity-20 bg-opacity-90 backdrop-blur-xl transition-transform transform scale-100 fade-up">
                     {children}
                     <button onClick={handleCloseModal} className="absolute top-7 right-7 dark:text-white focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
