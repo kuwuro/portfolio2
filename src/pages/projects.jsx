cc3ce7981b6f181b43abeb429800b041d68625f6
Enric Armengol <enarga@jviladoms.cat>
2024-08-15 17:41:01 +0200

    [ADD all tiles have their images (pc and mobile)

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -18,8 +18,8 @@ const projects = [
         animDelayMobile: '0.2s',
         title: 'DeltaShop',
         description: 'project2Description',
-        coverPC: 'https://images.unsplash.com/photo-1723242015501-2a52cfb64ed9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        coverMobile: "https://images.unsplash.com/photo-1723242015501-2a52cfb64ed9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        coverPC: 'https://www.enricarmengol.com/projectPictures/deltashop/deltashopMobile1.png',
+        coverMobile: "https://www.enricarmengol.com/projectPictures/deltashop/deltashopMobile1.png",
         link: 'project2Link',
         tech: ['tech1', 'tech2', 'tech3']
     },
@@ -28,8 +28,8 @@ const projects = [
         animDelayMobile: '0.3s',
         title: 'Desktopfolio',
         description: 'project3Description',
-        coverPC: 'https://images.unsplash.com/photo-1723242017539-39cd15eb75fd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        coverMobile: "https://images.unsplash.com/photo-1723242017539-39cd15eb75fd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        coverPC: 'https://www.enricarmengol.com/projectPictures/desktopfolio/desktopfolioPC1.png',
+        coverMobile: "https://www.enricarmengol.com/projectPictures/desktopfolio/desktopfolioMobile.png",
         link: 'project3Link',
         tech: ['tech1', 'tech2', 'tech3']
     },
@@ -38,8 +38,8 @@ const projects = [
         animDelayMobile: '0.4s',
         title: 'Delta Bark\'s',
         description: 'project4Description',
-        coverPC: 'https://images.unsplash.com/photo-1721225643686-cafccc8ba6f6?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        coverMobile: "https://images.unsplash.com/photo-1721225643686-cafccc8ba6f6?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        coverPC: 'https://www.enricarmengol.com/projectPictures/deltabarks/deltabarksPC3.png',
+        coverMobile: "https://www.enricarmengol.com/projectPictures/deltabarks/deltabarksPC3.png",
         link: 'project4Link',
         tech: ['tech1', 'tech2', 'tech3']
     },
@@ -48,8 +48,8 @@ const projects = [
         animDelayMobile: '0.5s',
         title: 'Psycomputers',
         description: 'project5Description',
-        coverPC: 'https://images.unsplash.com/photo-1722608274454-699ed706d7f5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        coverMobile: "https://images.unsplash.com/photo-1722608274454-699ed706d7f5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        coverPC: 'https://www.enricarmengol.com/projectPictures/psycomputers/psycomputersPC.png',
+        coverMobile: "https://www.enricarmengol.com/projectPictures/psycomputers/psycomputersMobile2.png",
         link: 'project5Link',
         tech: ['tech1', 'tech2', 'tech3']
     },
@@ -66,10 +66,11 @@ const projects = [
 ];
 
 function Projects({ darkMode }) {
+    const screenWidth = window.innerWidth;
     const { t } = useTranslation();
     return (
-        <div className="lg:mx-12 mx-8 lg:my-36 my-28 flex flex-col gap-8 justify-start items-start" style={{ height: `80vh` }}>
-            <h1 className="text-5xl font-poppins dark:text-white">{t('projectsTitle')}</h1>
+        <div className="lg:mx-12 mx-8 lg:my-36 my-24 flex flex-col lg:gap-8 gap-4 justify-start items-start" style={{ height: `${screenWidth < 1080 ? '73vh' : '80vh'}` }}>
+            <h1 className="lg:text-5xl text-4xl font-poppins dark:text-white">{t('projectsTitle')}</h1>
             <div id="gridTiles" className="grid lg:grid-rows-5 grid-cols-1 lg:grid-cols-6 gap-3 w-full lg:overflow-visible overflow-auto lg:mb-0 mb-8" style={{ height: `${window.innerWidth < 1080 ? 'auto' : '75vh'}` }}>
                 <ProjectTile project={projects[0]} gridCols={2} gridRows={5} style={{ animationDelay: `0.1s` }}/>
                 <ProjectTile project={projects[1]} gridCols={2} gridRows={2} style={{ animationDelay: `0.1s` }}/>
