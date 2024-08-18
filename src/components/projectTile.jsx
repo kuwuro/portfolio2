c8df446de553f02b4937eeee6885a59005bce15f
Enric Armengol <enarga@jviladoms.cat>
2024-08-18 03:17:18 +0200

    [ADD] new arrow to tiles

--- src/components/projectTile.jsx
+++ src/components/projectTile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import ProjectModal from "./projectModal.jsx";
+import LinkArrow from "../assets/icons/LinkArrow.svg";
 import { useTranslation } from "react-i18next";
 
 function ProjectTile({ project, gridCols, gridRows }) {
@@ -32,15 +33,18 @@ function ProjectTile({ project, gridCols, gridRows }) {
                 {modalOpen && <ProjectModal project={project} onClose={handleCloseModal} screenWidth={screenWidth}/>}
             </div>
             {screenWidth >= 1080 &&
-            <div className="absolute bottom-3 left-3 translate-y-16 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
-                <h1 className="text-3xl font-poppins text-white">{project.title}</h1>
-                <p className="text-md text-white">{t(project.description)}</p>
-                <div className="flex gap-2">
-                    {project.tech.map((tech, index) => (
-                        <p key={index} className="text-sm text-black bg-gray-200 px-2 py-1 rounded-lg">{tech}</p>
-                    ))}
+            <>
+                <img src={LinkArrow} alt="Link Arrow" className="h-10 w-10 absolute top-3 right-3 transform -translate-y-16 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300"/>
+                <div className="absolute bottom-3 left-3 translate-y-16 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
+                    <h1 className="text-3xl font-poppins text-white">{project.title}</h1>
+                    <p className="text-md text-white">{t(project.description)}</p>
+                    <div className="flex gap-2">
+                        {project.tech.map((tech, index) => (
+                            <p key={index} className="text-sm text-black bg-gray-200 px-2 py-1 rounded-lg">{tech}</p>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </>
             }
         </div>
     );
