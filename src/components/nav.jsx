ca33a4e293e6f8b1d928b20d119dfa99e8c81f9b
Enric Armengol <enarga@jviladoms.cat>
2024-08-29 17:05:29 +0200

    [ADD] changing styling of about me

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -20,15 +20,6 @@ function Nav({ currentTab, setCurrentTab }) {
                         <button>{t('projectsTitle')}</button>
                     </li>
                 )}
-                {currentTab === "about" ? (
-                    <li className="bg-white50 dark:bg-white20 py-2.5 px-4 rounded-3xl transition duration-300">
-                        <button className="cursor-default"><nobr>{t('aboutTitle')}</nobr></button>
-                    </li>
-                ) : (
-                    <li onClick={() => setCurrentTab("about")} className="cursor-pointer bg-transparent py-2.5 px-4 hover:bg-white30 hover:dark:bg-white10 rounded-3xl transition duration-300">
-                        <button><nobr>{t('aboutTitle')}</nobr></button>
-                    </li>
-                )}
                 {currentTab === "experience" ? (
                     <li className="bg-white50 dark:bg-white20 py-2.5 px-4 rounded-3xl transition duration-300">
                         <button className="cursor-default">{t('experienceTitle')}</button>
@@ -36,6 +27,15 @@ function Nav({ currentTab, setCurrentTab }) {
                 ) : (
                     <li onClick={() => setCurrentTab("experience")} className="cursor-pointer bg-transparent py-2.5 px-4 hover:bg-white30 hover:dark:bg-white10 rounded-3xl transition duration-300">
                         <button>{t('experienceTitle')}</button>
+                    </li>
+                )}
+                {currentTab === "about" ? (
+                    <li className="bg-white50 dark:bg-white20 py-2.5 px-4 rounded-3xl transition duration-300">
+                        <button className="cursor-default"><nobr>{t('aboutTitle')}</nobr></button>
+                    </li>
+                ) : (
+                    <li onClick={() => setCurrentTab("about")} className="cursor-pointer bg-transparent py-2.5 px-4 hover:bg-white30 hover:dark:bg-white10 rounded-3xl transition duration-300">
+                        <button><nobr>{t('aboutTitle')}</nobr></button>
                     </li>
                 )}
             </ul>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,11 +9,12 @@ function About({ darkMode }) {
     const screenWidth = window.innerWidth;
     const { t } = useTranslation();
     return (
-        <div className="lg:mx-12 mx-8 lg:my-36 my-24 flex flex-col lg:gap-8 gap-4 justify-start items-start" style={{ height: `${screenWidth < 1080 ? '73vh' : '77vh'}` }}>
-            <h1 className="lg:text-5xl text-4xl font-poppins dark:text-white">{t('aboutTitle')}</h1>
+        <div className="lg:mx-12 mx-8 lg:my-36 my-24 flex flex-col lg:gap-8 gap-4 justify-start items-start" style={{ height: `${screenWidth < 1080 ? '73vh' : '83vh'}` }}>
+            {/* <h1 className="lg:text-5xl text-4xl font-poppins dark:text-white">{t('aboutTitle')}</h1> */}
             <div className="grid lg:grid-cols-2 w-full lg:h-full">
-                <div className="flex justify-center items-center">
-                    <p className="font-dmsans dark:text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas non nemo voluptatum ipsa cupiditate porro vero. Laboriosam fuga tempore harum deserunt, consequuntur vitae, atque quos asperiores dolorem provident expedita assumenda.</p>
+                <div className="flex flex-col justify-center items-start">
+                    <h2 className="text-5xl font-poppins dark:text-white fade-up-text" style={{animationDelay: (screenWidth <= 1080) ? `0.8s` : `0s`}}>ENRIC ARMENGOL</h2>
+                    <p className="font-dmsans dark:text-white fade-up-text" style={{animationDelay: (screenWidth <= 1080) ? `0.9s` : `0.1s`}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas non nemo voluptatum ipsa cupiditate porro vero. Laboriosam fuga tempore harum deserunt, consequuntur vitae, atque quos asperiores dolorem provident expedita assumenda.</p>
                 </div>
                 <div className="flex justify-center items-center lg:order-last order-first lg:gap-8 gap-2 lg:mb-0 mb-4">
                     <div className="flex lg:flex-col lg:gap-8 gap-2">
