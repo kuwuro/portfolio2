f59839d51bb11ad9ddf4ed1c054d16f26e87956b
Enric Armengol <enarga@jviladoms.cat>
2024-08-29 21:27:31 +0200

    [FIX] mobile fix

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -16,7 +16,7 @@ const projects = [
     },
     {
         animationDelay: '0.5s',
-        animDelayMobile: '0.3s',
+        animDelayMobile: '0.4s',
         title: 'DeltaShop',
         description: 'deltashopDescription',
         coverPC: 'https://www.enricarmengol.com/projectPictures/deltashop/deltashopMobile1.png',
@@ -38,7 +38,7 @@ const projects = [
     },
     {
         animationDelay: '0.4s',
-        animDelayMobile: '0.4s',
+        animDelayMobile: '0.3s',
         title: 'Delta Bark\'s',
         description: 'deltabarksDescription',
         coverPC: 'https://www.enricarmengol.com/projectPictures/deltabarks/deltabarksPC3.png',
@@ -62,24 +62,14 @@ function Projects({ darkMode }) {
     const screenWidth = window.innerWidth;
     const { t } = useTranslation();
     return (
-        <div className="lg:mx-12 mx-8 lg:my-36 my-24 flex flex-col lg:gap-8 gap-4 justify-start items-start lg:overflow-visible overflow-auto overflow-x-hidden" style={{ height: `${screenWidth < 1080 ? '100vh' : '77vh'}` }}>
+        <div className="lg:mx-12 mx-8 lg:my-36 my-24 flex flex-col lg:gap-8 gap-4 justify-start items-start" style={{ height: `${screenWidth < 1080 ? '73vh' : '77vh'}` }}>
             <h1 className="lg:text-5xl text-4xl font-poppins dark:text-white">{t('projectsTitle')}</h1>
-            <div id="gridTiles" className="grid lg:grid-rows-5 grid-cols-1 lg:grid-cols-6 gap-3 w-full lg:mb-0 mb-52" style={{ height: `${window.innerWidth < 1080 ? 'auto' : '75vh'}` }}>
+            <div id="gridTiles" className="grid lg:grid-rows-5 grid-cols-1 lg:grid-cols-6 gap-3 w-full lg:overflow-visible overflow-auto overflow-x-hidden lg:mb-0 mb-8" style={{ height: `${window.innerWidth < 1080 ? 'auto' : '75vh'}` }}>
                 <ProjectTile project={projects[0]} gridCols={2} gridRows={5}/>
                 <ProjectTile project={projects[2]} gridCols={2} gridRows={3}/>
-                {(screenWidth >= 1080) ? (
-                    <>
-                        <ProjectTile project={projects[3]} gridCols={2} gridRows={2}/>
-                        <ProjectTile project={projects[1]} gridCols={2} gridRows={3}/>
-                        <ProjectTile project={projects[4]} gridCols={2} gridRows={2}/>
-                    </>
-                ) : (
-                    <>
-                        <ProjectTile project={projects[1]} gridCols={2} gridRows={3}/>
-                        <ProjectTile project={projects[3]} gridCols={2} gridRows={2}/>
-                        <ProjectTile project={projects[4]} gridCols={2} gridRows={2}/>
-                    </>
-                )}
+                <ProjectTile project={projects[3]} gridCols={2} gridRows={2}/>
+                <ProjectTile project={projects[1]} gridCols={2} gridRows={3}/>
+                <ProjectTile project={projects[4]} gridCols={2} gridRows={2}/>
             </div>
         </div>
     );
